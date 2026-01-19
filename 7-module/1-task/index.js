b726c7471d69e142355ebb6db2d02ebde5b1363d
Danil Tukcurin <tukcurinsaad@gmail.com>
2026-01-19 23:28:04 +0300

    Решил все задачи седьмого модуля

--- 7-module/1-task/index.js
+++ 7-module/1-task/index.js
@@ -3,5 +3,83 @@ import createElement from '../../assets/lib/create-element.js';
 export default class RibbonMenu {
   constructor(categories) {
     this.categories = categories;
+    this.elem = this.render();
+  }
+  render() {
+
+    let categoriesHtml = this.categories.map((category) => `
+    <a href="#" class="ribbon__item" data-id="${category.id}">${category.name}
+    </a>
+    `).join('');
+    let ribbon = createElement(`
+      <div class="ribbon">
+        <button class="ribbon__arrow ribbon__arrow_left ribbon__arrow_visible">
+          <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+        </button>
+      <nav class="ribbon__inner">
+        ${categoriesHtml}
+      </nav>
+        <button class="ribbon__arrow ribbon__arrow_right">
+          <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+        </button>
+      </div>
+      `);
+
+    let ribbonInner = ribbon.querySelector('.ribbon__inner');
+    let arrowLeft = ribbon.querySelector('.ribbon__arrow_left');
+    let arrowRight = ribbon.querySelector('.ribbon__arrow_right');
+    arrowRight.addEventListener('click', () => {
+      ribbonInner.scrollBy(350, 0);
+    });
+    arrowLeft.addEventListener('click', () => {
+      ribbonInner.scrollBy(-350, 0);
+    });
+
+    ribbonInner.addEventListener('scroll', () => {
+      let scrollLeft = ribbonInner.scrollLeft;
+      let scrollWidth = ribbonInner.scrollWidth;
+      let clientWidth = ribbonInner.clientWidth;
+
+      let scrollRight = scrollWidth - scrollLeft - clientWidth;
+
+
+      if (scrollLeft === 0) {
+        arrowLeft.classList.remove('ribbon__arrow_visible');
+      } else {
+        arrowLeft.classList.add('ribbon__arrow_visible');
+      }
+      if (scrollRight < 1) {
+        arrowRight.classList.remove('ribbon__arrow_visible');
+      } else {
+        arrowRight.classList.add('ribbon__arrow_visible');
+      }
+    });
+    ribbonInner.dispatchEvent(new Event('scroll'));
+    let ribbonLinks = ribbon.querySelectorAll('.ribbon__item');
+    ribbonLinks[0].classList.add('ribbon__item_active');
+    ribbonLinks.forEach((link) => {
+      link.addEventListener('click', (event) => {
+        event.preventDefault();
+        let currentActive = ribbon.querySelector('.ribbon__item_active');
+        if (currentActive && currentActive !== link) {
+          currentActive.classList.remove('ribbon__item_active');
+        }
+        link.classList.add('ribbon__item_active');
+        let customEvent = new CustomEvent('ribbon-select', {
+          detail: link.dataset.id,
+          bubbles: true
+        });
+        ribbon.dispatchEvent(customEvent);
+      })
+
+
+    })
+
+
+
+
+    return ribbon;
+
+
   }
 }
--- 7-module/2-task/index.js
+++ 7-module/2-task/index.js
@@ -2,5 +2,61 @@ import createElement from '../../assets/lib/create-element.js';
 
 export default class Modal {
   constructor() {
+   this.elem = createElement(`
+     <div class="modal">
+      <!--Прозрачная подложка перекрывающая интерфейс-->
+      <div class="modal__overlay"></div>
+  
+      <div class="modal__inner">
+        <div class="modal__header">
+          <!--Кнопка закрытия модального окна-->
+          <button type="button" class="modal__close">
+            <img src="/assets/images/icons/cross-icon.svg" alt="close-icon" />
+          </button>
+  
+          <h3 class="modal__title">
+            Вот сюда нужно добавлять заголовок
+          </h3>
+        </div>
+  
+        <div class="modal__body">
+          A сюда нужно добавлять содержимое тела модального окна
+        </div>
+      </div>
+    </div>
+    `);
+    this.keydown = (event) => {
+    if (event.code === 'Escape') {
+      this.close();
+    }
+  }
+  }
+  
+  open() {
+    document.body.append(this.elem);
+    document.body.classList.add('is-modal-open');
+    this.closeButton = this.elem.querySelector('.modal__close');
+    this.closeClick = (event) => this.close();
+    this.closeButton.addEventListener('click', this.closeClick);
+    document.addEventListener('keydown', this.keydown );
+    }
+    
+  close () {
+    document.body.classList.remove('is-modal-open');
+    document.removeEventListener('keydown', this.keydown);
+    this.closeButton.removeEventListener('click', this.closeClick);
+    this.elem.remove();
+  }
+  setTitle (titleText) {
+    this.title = this.elem.querySelector('.modal__title');
+    this.title.textContent = titleText;
+
+  }
+
+  setBody (node) {
+   this.modalBody = this.elem.querySelector('.modal__body');
+   this.modalBody.innerHTML = "";
+   this.modalBody.append(node);
+
   }
 }
